4bbc81a5f6038af11c0677357e009059c220e6d8
CommanderRedYT <ccommanderred@gmail.com>
2026-04-23 17:06:22 +0200

    Properly set NODE_ENV and log with timestamps

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ if (!DATA_DIR) {
 
 const app = express();
 
-app.use(morgan('dev'));
+app.use(morgan(process.env.NODE_ENV === 'production' ? 'combined' : 'dev'));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
